40514d66007a8de68e9701dbb99b5eff1cdd492c
Gaurav Agnihotri <gauravrsys@gmail.com>
2017-11-28 22:15:59 +0530

    added dataprovider for tweets

--- src/test/java/com/twitter/tests/HomepageTest.java
+++ src/test/java/com/twitter/tests/HomepageTest.java
@@ -3,11 +3,13 @@ package com.twitter.tests;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import com.twitter.base.TwitterBase;
 import com.twitter.pages.Homepage;
 import com.twitter.pages.Loginpage;
+import com.twitter.utility.TestUtil;
 
 public class HomepageTest extends TwitterBase {
 	
@@ -45,6 +47,30 @@ public class HomepageTest extends TwitterBase {
 	public void tearDown()
 	{
 		TwitterBase.logout();
+	}
+	
+	@DataProvider(name="TweetData")
+
+	public Object[][] getData()
+	{
+		
+		TestUtil util= new TestUtil(prop.getProperty("ExcelPath"));
+		
+		int rcount= util.getRowCount(1);
+		int ccount= util.getColCount(1);
+		
+		Object[][] arr= new Object[rcount-1][ccount];
+		
+		for (int i=1;i<rcount;i++)
+		{
+			for(int j=0;j<ccount;j++)
+			{
+				arr[i-1][j]= util.getExcelData(1,i,j);
+			}	
+		}
+		
+		return arr;
+		
 	}
 	
 
